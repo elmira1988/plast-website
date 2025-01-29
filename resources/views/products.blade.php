5cd8685208547c4d99503c8636879e9e11f68e12
elmira1988 <elmira.sharapova@yandex.ru>
2025-01-29 19:59:49 +0500

    Изменили название Производство пчеловодного инвентаря

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -57,7 +57,7 @@
                                 <div class="solution_card">
                                     <div class="hover_color_bubble"></div>
                                     <a class="solu_title row align-items-center" href="https://strbee.biz/katalog?tfc_storepartuid[455923084]=%D0%9F%D1%87%D1%91%D0%BB%D0%BE%D0%B8%D0%BD%D0%B2%D0%B5%D0%BD%D1%82%D0%B0%D1%80%D1%8C&tfc_div=:::">
-                                        <h3 class="col-lg-12 text-center">Комплектующие для медогонок</h3>
+                                        <h3 class="col-lg-12 text-center">Производство пчеловодного инвентаря</h3>
                                     </a>
                                 </div>
                             </div>
